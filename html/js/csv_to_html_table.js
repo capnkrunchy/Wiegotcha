8a416b3a1245d2a167dd6839487d73d501dbbf91
Ryan Hilliker <rhilliker@carboncreditsolutions.ca>
2019-07-16 21:46:36 -0600

    added Simulate and Clone buttons

--- html/js/csv_to_html_table.js
+++ html/js/csv_to_html_table.js
@@ -1,42 +1,49 @@
 function init_table(options) {
 
-  options = options || {};
-  var csv_path = options.csv_path || "";
-  var el = options.element || "table-container";
-  var allow_download = options.allow_download || false;
-  var csv_options = options.csv_options || {};
-  var datatables_options = options.datatables_options || {};
+    options = options || {};
+    var csv_path = options.csv_path || "";
+    var el = options.element || "table-container";
+    var allow_download = options.allow_download || false;
+    var csv_options = options.csv_options || {};
+    var datatables_options = options.datatables_options || {};
 
-  $("#" + el).html("<table class='table table-striped table-condensed' id='my-table'></table>");
+    $("#" + el).html("<table class='table table-striped table-condensed' id='my-table'></table>");
 
-  $.when($.get(csv_path)).then(
-    function(data){      
-      var csv_data = $.csv.toArrays(data, csv_options);
+    $.when($.get(csv_path)).then(
+        function (data) {
+            var csv_data = $.csv.toArrays(data, csv_options);
 
-      var table_head = "<thead><tr>";
+            var table_head = "<thead><tr>";
 
-      for (head_id = 0; head_id < csv_data[0].length; head_id++) { 
-        table_head += "<th>" + csv_data[0][head_id] + "</th>";
-      }
+            for (head_id = 0; head_id < csv_data[0].length; head_id++) {
+                table_head += "<th>" + csv_data[0][head_id] + "</th>";
+            }
 
-      table_head += "</tr></thead>";
-      $('#my-table').append(table_head);
-      $('#my-table').append("<tbody></tbody>");
+            table_head += "<th>Custom Buttons</th>";
 
-      for (row_id = 1; row_id < csv_data.length; row_id++) { 
-        var row_html = "<tr>";
+            table_head += "</tr></thead>";
+            $('#my-table').append(table_head);
+            $('#my-table').append("<tbody></tbody>");
 
-          for (col_id = 0; col_id < csv_data[row_id].length; col_id++) { 
-            row_html += "<td>" + csv_data[row_id][col_id] + "</td>";
-          }
-          
-        row_html += "</tr>";
-        $('#my-table tbody').append(row_html);
-      }
+            for (row_id = 1; row_id < csv_data.length; row_id++) {
+                var row_html = "<tr>";
 
-      $("#my-table").DataTable(datatables_options);
+                for (col_id = 0; col_id < csv_data[row_id].length; col_id++) {
+                    row_html += "<td>" + csv_data[row_id][col_id] + "</td>";
+                }
 
-      if (allow_download)
-        $("#" + el).append("<p><a class='btn btn-info' href='" + csv_path + "'><i class='glyphicon glyphicon-download'></i> Download as CSV</a></p>");
-    });
+                row_html += '<td> \
+                                <a class="button" href = "/cgi-bin/simulatecard.sh?' + csv_data[row_id][4] + '" target = "_blank" >Simulate</a> \
+                                <a href = "/cgi-bin/writecard.sh?' + csv_data[row_id][4] + '" target = "_blank" >Clone</a> \
+                            </td > ';
+
+                row_html += "</tr>";
+                $('#my-table tbody').append(row_html);
+            }
+
+            $("#my-table").DataTable(datatables_options);
+
+            if (allow_download)
+                $("#" + el).append("<p><a class='btn btn-info' href='" + csv_path + "'><i class='glyphicon glyphicon-download'></i> Download as CSV</a></p>");
+        });
 }
